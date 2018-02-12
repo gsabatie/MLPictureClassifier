c83c4582f03479e1a7a3e1ce1ddb56928414818d
Guillaume <guillaume-heritiana.sabatie@epitech.eu>
2018-02-13 00:40:04 +0100

    added allow unauthenticated

--- Install.sh
+++ Install.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 # update apt-get
- apt-get update
+ apt-get update 
 # Install apt-get deps
- sudo apt-get install openjdk-8-jdk git python-dev python3-dev python-numpy python3-numpy build-essential python-pip python3-pip python-virtualenv swig python-wheel libcurl3-dev  
+ sudo apt-get install openjdk-8-jdk git python-dev python3-dev python-numpy python3-numpy build-essential python-pip python3-pip python-virtualenv swig python-wheel libcurl3-dev -y 
 
 #install nvidia drivers
 curl -O "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb"
 dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 apt-get update
 #  Added this to make sure we don't drag down the newest version of cuda!
-apt-get install cuda-9-0 -y
+apt-get install cuda-9-0 -y --allow-unauthenticated
 
-check nvidia driver install
+#check nvidia driver install
 nvidia-smi   
 
 #Install cudnn
-CUDNN_DOWNLOAD_SUM=1a3e076447d5b9860c73d9bebe7087ffcb7b0c8814fd1e506096435a2ad9ab0e
-curl -fsSL http://developer.download.nvidia.com/compute/redist/cudnn/v7.0.5/cudnn-9.0-linux-x64-v7.tgz -O
-echo "$CUDNN_DOWNLOAD_SUM cudnn-9.0-linux-x64-v7.tgz" | sha256sum -c - 
+wget  http://developer.download.nvidia.com/compute/redist/cudnn/v7.0.5/cudnn-9.0-linux-x64-v7.tgz 
 sudo tar -xzvf  cudnn-9.0-linux-x64-v7.tgz 
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
@@ -29,8 +27,8 @@ echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
 
 source ~/.bashrc
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh   
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+bash Miniconda3-latest-Linux-x86_64.sh -b
 
 # press s to skip terms   
 
